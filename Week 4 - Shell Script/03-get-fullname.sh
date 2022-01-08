aea5f29b390758e0181374b70da3da4918836df7
KanonKC <kanonkce@gmail.com>
2022-01-09 01:09:20 +0700

    Deleted

--- Week 4 - Shell Script/03-get-fullname.sh
+++
@@ -1,5 +0,0 @@
-result=$(cat /etc/passwd | grep -w $1)
-
-fullname=$(echo $(echo $result | cut -d':' -f5) | cut -d',' -f1)
-
-echo $fullname
--- Week 4 - Shell Script/05-count-fails.sh
+++
@@ -1,4 +0,0 @@
-access=$(cat access.log | grep -w $1 | wc -l)
-success=$(cat success.log | grep -w $1 | wc -l)
-fail=$((access-success))
-echo $fail
