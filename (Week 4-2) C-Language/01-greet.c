22401b2375538fcdd2729800ca1b7d345b7ea5cb
KanonKC <kanonkce@gmail.com>
2022-01-09 01:28:32 +0700

    2/10

---
+++ (Week 4-2) C-Language/01-greet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello, ");
+  printf("how are you?\n");
+  printf("I'm fine.\nGoodbye.\n");
+
+  return 0;
+}
+
+// gcc -o greet 01-greet.c
+// ตอนส่งเอา 01- ออก
