959688af9d049a79076720fc845b2138239f3837
KanonKC <kanonkce@gmail.com>
2022-01-09 02:16:17 +0700

    9/10

---
+++ (Week 4-2) C-Language/09-example.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define IS_VALID(x)  ((50<=x) && (x<=200))
+#define VALID_MSG    "Good value.\n"
+#define INVALID_MSG  "Bad value.\n"
+
+int main()
+{
+  int val;
+
+  printf("Enter a value: ");
+  scanf("%d", &val);
+  if (IS_VALID(val))
+    printf(VALID_MSG);
+  else
+    printf(INVALID_MSG);
+
+  return 0;
+}
+
+// Answer -> resource/09/exampleE:471
+
