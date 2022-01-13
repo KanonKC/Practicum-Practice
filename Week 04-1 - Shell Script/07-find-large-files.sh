6336867ddb2f4770db7270e29f99ec48d949c1a3
KanonKC <kanonkce@gmail.com>
2022-01-13 14:14:15 +0700

    8/10

--- Week 04-1 - Shell Script/07-find-large-files.sh
+++ Week 04-1 - Shell Script/07-find-large-files.sh
@@ -1,10 +1,12 @@
-size=$(ls -l | tr -s ' ' | grep '^-' | cut -d' ' -f5)
-name=$(ls -l | tr -s ' ' | grep '^-' | cut -d' ' -f9)
-
-if [ $# = 0 ]; then
+if [ $# != 1 ]; then
     echo "Usage: find-large-files.sh <size>"
     exit 1
 fi
-for i in $result; do
-    echo $i
+
+for i in $(ls -l | tr -s ' ' | cut -d' ' -f5,9 | tr ' ' ':'); do
+    size=$(echo "$i" | cut -d: -f1)
+    filename=$(echo "$i" | cut -d: -f2)
+    if [ $((size)) -ge $1 ]; then
+        echo $filename
+    fi
 done
